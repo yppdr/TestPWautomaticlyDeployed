0a653a9f9446b78dd82ce77c1e94697a8749a859
Yannis Piot Pilot <yannis@piotpilot.eu>
2021-11-05 01:10:00 +0100

    gitflow-feature-stash: count

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <Html>  
 <Head>
 <title>
-Example of make a text B,I,U
+Yannis Piot Pilot - piotpilot.eu
 </title>
 </Head>
 <Body> 
-<b> [This text is Bold......] </b>
-<I> [This text is Italic......] </I>
-<U> [This text is Underline......] </U> 
-TEST
-test
-Test
+Go to piotpilot.eu
 
-<?= "TEST PHP" ?>
+<?php
+
+$filename = "counter.txt"; // This is at root of the file using this script.
+$fd = fopen ($filename, "r"); // opening the file counter.txt in read mode
+$contents = fread ($fd, filesize($filename)); // reading the content of the file
+fclose ($fd); // Closing the file pointer
+$contents=$contents+1; // incrementing the counter value by one
+echo $contents; // printing the incremented counter value
+/*
+The above code will do the reading and displaying the counter to the screen, now we have to store the above value in the same counter.txt file by overwriting the old data with the new counter data. We will open the counter.txt file in write mode and then write to it.
+*/
+
+$fp = fopen ($filename, "w"); // Open the file in write mode
+fwrite ($fp,$contents); // Write the new data to the file
+fclose ($fp); // Closing the file pointer 
+
+?>
 
 </Body>
 </Html>
